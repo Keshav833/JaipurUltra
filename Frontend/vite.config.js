87ba0702a354a3e84d2f3a678eb5f1963b578336
keshu <korplz1408@gmail.com>
2026-05-02 15:23:32 +0530

    fixed deploy error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,13 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'vendor-react': ['react', 'react-dom'],
-          'vendor-ui': ['gsap', 'lenis'],
+  rollupOptions: {
+    output: {
+      manualChunks(id) {
+        if (id.includes('node_modules/react') || 
+            id.includes('node_modules/react-dom')) {
+          return 'vendor';
+        }
+        if (id.includes('node_modules/leaflet')) {
+          return 'leaflet';
         }
       }
     }
   }
+}
 })
